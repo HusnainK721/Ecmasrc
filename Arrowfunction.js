0fc0765333b7c8bf11d8b4899b7a3c546e396e4b
HusnainK721 <husnaink721@gmail.com>
2021-07-14 19:11:55 +0530

    arrow function

---
+++ Arrowfunction.js
@@ -0,0 +1,14 @@
+const addnumber=(num1, num2)=> num1+num2;
+const subnumber=(num1, num2)=>num1-num2
+const multiple=(num1 ,num2)=>num1*num2
+
+console.log(addnumber(5,8))
+
+console.log("sum of given number are"+addnumber(12,65))
+console.log("sub of given number are"+subnumber(12,65))
+console.log("mul of given number are"+multiple(12,5))
+
+
+
+console.log("sum of 10 and 65 is"+addnumber(10,65))
+
