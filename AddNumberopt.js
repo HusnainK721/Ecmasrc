2235d4dda85ab7fd4b5430e0a00e3d8ae82a05be
HusnainK721 <husnaink721@gmail.com>
2021-07-14 19:10:55 +0530

    add num ES

---
+++ AddNumberopt.js
@@ -0,0 +1,12 @@
+function addnumber(num1, num2){
+    return num1+num2;
+}
+
+console.log(addnumber(5,8))
+
+console.log("sum of given number are"+addnumber(12,65))
+
+
+
+console.log("sum of 10 and 65 is"+addnumber(10,65))
+
