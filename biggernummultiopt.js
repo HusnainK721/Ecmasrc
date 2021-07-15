d1e74c86823c12a36c80a0f647783e2d468fbcfd
HusnainK721 <husnaink721@gmail.com>
2021-07-15 12:25:04 +0530

    bigggernumopt ES

---
+++ biggernummultiopt.js
@@ -0,0 +1,19 @@
+// const biggernummultiopt = (num1,num2,num3,num4)=>{
+// let bignum=0;
+// (num1>num2) ? num1 : num2
+
+// bignum = bignum > num3 ? bignum : num3;
+
+// bignum = bignum > num4 ? bignum : num4;
+
+// return bignum
+// };
+// console.log(biggernummultiopt(10,20,60,80))
+
+
+
+
+const biggernummultiopt = (num1,num2,num3,num4,num5,num6)=>
+Math.max(num1,num2,num3,num4,num5,num6)
+
+console.log(biggernummultiopt(10,20,60,80,50,650))
