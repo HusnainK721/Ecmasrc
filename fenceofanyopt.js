15ed2619d338efb59921a44b7188563435dac908
HusnainK721 <husnaink721@gmail.com>
2021-07-17 12:35:23 +0530

    fence of any Opt ES

---
+++ fenceofanyopt.js
@@ -0,0 +1,9 @@
+const fenceforanyopt = (sides ,nummofwire,rate) => {
+    let sum=0;
+    sides.forEach(element => {
+        sum=sum + element;
+    });
+    return sum *nummofwire*rate;
+}
+console.log(`sum of fence ${fenceforanyopt([10,20,40,15,54,28]),51,30]})`;
+
