0146fe2d6625a8c07c33afd132f8b0c1774a3969
HusnainK721 <husnaink721@gmail.com>
2021-08-23 15:23:35 +0530

    Addition of array

---
+++ addTwoArray.js
@@ -0,0 +1,7 @@
+const totalarr = (arr1,arr2)=>arr1.map((item,index)=>item+arr2[index]);
+
+console.log(totalarr([1,2,3,4],[5,6,7,8]))
+
+
+
+//------------------------MAP----METHOD------------------//
