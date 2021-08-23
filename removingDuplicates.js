5059ea925609d65a9b19975a34efba753bf25604
HusnainK721 <husnaink721@gmail.com>
2021-08-23 15:30:27 +0530

    Removal of duplicates

---
+++ removingDuplicates.js
@@ -0,0 +1,10 @@
+const removeduplicate = (mylorem)=>{
+    const newStr = mylorem.split(" ");
+    const myele = [];
+    newStr.forEach((element,index) => {
+        newStr.indexOf(element) !== index && myele.push(element);
+    });
+    const uniqueStr = [...new Set(myele)]
+    return uniqueStr
+}
+console.log(removeduplicate("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))
