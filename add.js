1cc5cc1e8c52a8d4f9cf303b6393248ea65517ae
HusnainK721 <husnaink721@gmail.com>
2021-08-24 12:06:25 +0530

    new

--- add.js
+++ add.js
@@ -1,12 +1,13 @@
-// function addme(num1, num2){
-// var sum=num1 + num2;
-// console.log("sum of " +num1, "and " +  num2 +"   is equal to " + sum);
-// }
+function addme(num1, num2){
+var sum=num1 + num2;
+console.log( sum);
+}
 
-// addme(15, 50);
-// addme(250, 684);
-// addme(5544,421);
+addme(15, 50);
+addme(250, 684);
+addme(5544,421);
 
-var abc={husnain}
+const add=(num1,num2)=>num1+num2
 
-console.log(abc)
+
+console.log(add(5,5))
