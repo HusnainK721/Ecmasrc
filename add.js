5501242be60d5619a7575958612226a73b8c3396
HusnainK721 <husnaink721@gmail.com>
2021-08-26 16:12:15 +0530

    add

--- add.js
+++ add.js
@@ -1,11 +1,12 @@
 function addme(num1, num2){
 var sum=num1 + num2;
-console.log( sum);
+console.log("sum of " +num1, "and " +  num2 +"   is equal to " + sum);
 }
 
 addme(15, 50);
 addme(250, 684);
 addme(5544,421);
+//------------OPT--------------//
 
 const add=(num1,num2)=>num1+num2
 
