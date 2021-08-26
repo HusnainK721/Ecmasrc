fb99434fed75ee39c934dfea0ea1bef444770c90
HusnainK721 <husnaink721@gmail.com>
2021-08-26 16:13:15 +0530

    joining of Array

---
+++ joiningArray.js
@@ -0,0 +1,19 @@
+//--------.Join---------------//
+
+const arr = ["A","M","R"];
+
+
+// console.log(arr.join(""))
+//---------------------------------forEach-----------------------------------//
+
+// let txt ="";
+
+// arr.forEach((element) => {
+//     txt+= element
+// });
+
+// console.log(txt)
+
+//-------------------Reduce -----Method-------------//
+
+console.log(arr.reduce((a,b)=>a+b))
